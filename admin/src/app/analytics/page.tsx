7d42039d8a2bba535e49c6841b811368c243405c
Wade Sultan <wadesultan@gmail.com>
2026-06-14 23:09:48 -0700

    Forcing dynamic from admin pages

--- admin/src/app/analytics/page.tsx
+++ admin/src/app/analytics/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { db } from '@/lib/prisma';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import {
--- admin/src/app/cpus/page.tsx
+++ admin/src/app/cpus/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { db } from '@/lib/prisma';
 import { CpuTable } from './client';
 
